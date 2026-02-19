f32bd64c900c522673606cfafb49825681a43f1d
justaguyfrom92 <justaguyfrom92@gmail.com>
2026-02-18 23:54:44 -0600

    Delete RegularDashboard directory

--- RegularDashboard/Models/DashboardViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace RegularDashboard.Models;
-
-public class DashboardViewModel
-{
-    public IReadOnlyList<StatCard> StatCards { get; set; } = [];
-}
-
-public record StatCard(string Label, string Value, string Delta, bool IsPositive);
